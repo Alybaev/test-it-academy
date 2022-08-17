87de65bf553b2f38de37dd17cb929a17428609dc
Dastan Alybaev <dastan.alybaev@kandasoft.com>
2022-08-17 20:49:57 +0600

    Change y variabler to 0

--- Test/Program.cs
+++ Test/Program.cs
@@ -5,7 +5,7 @@ public class Program
     public static void Main(string[] args)
     {
         var x = 1;
-        var y = 1;
+        var y = 0;
         var calc = new Calculator();
         var result = calc.Divide(x, y);
         Console.WriteLine(result); 
