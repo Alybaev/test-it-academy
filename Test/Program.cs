e9f529c0c1ec14bed24b97c5d0a50014c72c68e3
Dastan Alybaev <dastan.alybaev@kandasoft.com>
2022-08-17 20:48:06 +0600

    Initial commit

---
+++ Test/Program.cs
@@ -0,0 +1,15 @@
+﻿using Test;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var x = 1;
+        var y = 1;
+        var calc = new Calculator();
+        var result = calc.Divide(x, y);
+        Console.WriteLine(result); 
+    }
+
+    
+}
